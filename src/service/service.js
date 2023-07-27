47ff63f41ec97ccc1ade2696b28c162816b0942b
CuongVnZ <edgcuong@gmail.com>
2023-07-27 16:58:00 +0700

    Update

--- src/service/service.js
+++ src/service/service.js
@@ -4,13 +4,13 @@ import VotingContract from './abi/VotingContract.json'
 export const tokenContractInstance = (web3) => {
     return new web3.eth.Contract(
         TokenContract, // abi of SC voting token
-        "0xF0A5133A84aB50519065eF081C82e9eC3904eb89" // address of Voting token
+        "0x4E37E0fC6d36526986FA50734ED72036E59432c3" // address of Voting token
     )
 }
 
 export const votingContractInstance = web3 => {
     return new web3.eth.Contract(
         VotingContract, // abi of SC governance contract
-        "0xCD4320770EA4BcbDC8dD78FFEBa0e9Ac5154BA10"  // address of governance contract
+        "0x659e0bf859c8d4B60A866fa1CA9E1b76a965Acea"  // address of governance contract
     )
 }
